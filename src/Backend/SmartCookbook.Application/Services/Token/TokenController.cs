5aaf04a9e64b92784f118f305387d01dc1fbc9b2
Rafael Telles <rafast.telles@gmail.com>
2022-09-20 16:46:15 -0300

    add filtro exception e DI

---
+++ src/Backend/SmartCookbook.Application/Services/Token/TokenController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace SmartCookbook.Application.Services.Token;
+
+public class TokenController
+{
+    private const string EmailAlias = "eml";
+    private readonly double _expirationTimeInMinutes;
+    private readonly string _securityKey;
+
+    public TokenController(double expirationTimeInMinutes, string securityKey)
+    {
+        _expirationTimeInMinutes = expirationTimeInMinutes;
+        _securityKey = securityKey;
+    }
+
+    public string TokenGen(string userEmail)
+    {
+        var claims = new List<Claim>
+        {
+            new Claim(EmailAlias, userEmail)
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.AddMinutes(_expirationTimeInMinutes),
+            SigningCredentials = new SigningCredentials(SimetricKey(), SecurityAlgorithms.HmacSha256Signature)
+        };
+
+        var securityToken = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(securityToken);
+    }
+
+    public void ValidateToken(string token)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        var validationParams = new TokenValidationParameters
+        {
+            RequireExpirationTime = true,
+            IssuerSigningKey = SimetricKey(),
+            ClockSkew = new TimeSpan(0),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+
+        tokenHandler.ValidateToken(token, validationParams, out _);
+    }
+
+    private SymmetricSecurityKey SimetricKey()
+    {
+        var symmetricKey = Convert.FromBase64String(_securityKey);
+        return new SymmetricSecurityKey(symmetricKey);
+    }
+}
---
+++ src/Backend/SmartCookbook.Application/UseCases/User/Register/IRegisterUserUseCase.cs
@@ -0,0 +1,9 @@
+﻿using SmartCookbook.Comunicacao.Request;
+using SmartCookbook.Comunicacao.Response;
+
+namespace SmartCookbook.Application.UseCases.User.Register;
+
+public interface IRegisterUserUseCase
+{
+    Task<ResponseUserRegisteredJson> Execute(RequestRegisterUserJson request);
+}
