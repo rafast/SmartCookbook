7c28312ce5a8ae9956cb27dc4ac77a917f7cda3b
Rafael Telles <rafast.telles@gmail.com>
2022-09-21 18:12:30 -0300

    Criando testes de unidade para o validator de registrar usuario

---
+++ tests/TestsUtility/Requests/RegisterUserRequestBuilder.cs
@@ -0,0 +1,16 @@
+﻿using Bogus;
+using SmartCookbook.Comunicacao.Request;
+
+namespace TestsUtility.Requests;
+
+public class RegisterUserRequestBuilder
+{
+    public static RequestRegisterUserJson Build(int passwordLength = 10)
+    {
+        return new Faker<RequestRegisterUserJson>()
+            .RuleFor(c => c.Name, f => f.Person.FullName)
+            .RuleFor(c => c.Email, f => f.Internet.Email())
+            .RuleFor(c => c.Password, f => f.Internet.Password(passwordLength))
+            .RuleFor(c => c.Phone, f => f.Phone.PhoneNumber("## ! ####-####").Replace("!", $"{f.Random.Int(min: 1, max: 9)}"));
+    }
+}
---
+++ tests/Validators.Test/User/Register/RegisterUserValidatorTest.cs
@@ -0,0 +1,126 @@
+﻿using FluentAssertions;
+using SmartCookbook.Application.UseCases.User.Register;
+using SmartCookbook.Exceptions;
+using TestsUtility.Requests;
+using Xunit;
+
+namespace Validators.Test.User.Register;
+
+public class RegisterUserValidatorTest
+{
+    [Fact]
+    public void Validate_Success()
+    {
+        var validator = new RegisterUserValidator();
+
+        var request = RegisterUserRequestBuilder.Build();
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeTrue();      
+    }
+
+    [Fact]
+    public void Validate_Error_Empty_Name()
+    {
+        var validator = new RegisterUserValidator();
+
+        var request = RegisterUserRequestBuilder.Build();
+        request.Name = string.Empty;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceErrorMessages.EMPTY_USER_NAME));
+    }
+
+    [Fact]
+    public void Validate_Error_Empty_Email()
+    {
+        var validator = new RegisterUserValidator();
+
+        var request = RegisterUserRequestBuilder.Build();
+        request.Email = string.Empty;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceErrorMessages.EMPTY_USER_EMAIL));
+    }
+
+    [Fact]
+    public void Validate_Error_Empty_Password()
+    {
+        var validator = new RegisterUserValidator();
+
+        var request = RegisterUserRequestBuilder.Build();
+        request.Password = string.Empty;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceErrorMessages.EMPTY_USER_PASSWORD));
+    }
+
+    [Fact]
+    public void Validate_Error_Empty_Phone()
+    {
+        var validator = new RegisterUserValidator();
+
+        var request = RegisterUserRequestBuilder.Build();
+        request.Phone = string.Empty;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceErrorMessages.EMPTY_USER_PHONE));
+    }
+
+    [Fact]
+    public void Validate_Error_Invalid_Email()
+    {
+        var validator = new RegisterUserValidator();
+
+        var request = RegisterUserRequestBuilder.Build();
+        request.Email = "email";
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceErrorMessages.INVALID_USER_EMAIL));
+    }
+
+    [Fact]
+    public void Validate_Error_Invalid_Phone()
+    {
+        var validator = new RegisterUserValidator();
+
+        var request = RegisterUserRequestBuilder.Build();
+        request.Phone = "32 2231";
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceErrorMessages.INVALID_USER_PHONE));
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    [InlineData(4)]
+    [InlineData(5)]
+    public void Validate_Error_Invalid_Password(int passwordLength)
+    {
+        var validator = new RegisterUserValidator();
+
+        var request = RegisterUserRequestBuilder.Build(passwordLength);
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceErrorMessages.INVALID_USER_PASSWORD));
+    }
+
+
+}
