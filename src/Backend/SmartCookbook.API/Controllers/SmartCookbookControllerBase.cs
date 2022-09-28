1a5caf80979e089f3e1aa92ca17ee624edbfefbf
Rafael Telles <rafast.telles@gmail.com>
2022-09-28 18:00:08 -0300

    Add endpoint para realizar o login na API

---
+++ src/Backend/SmartCookbook.API/Controllers/SmartCookbookControllerBase.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmartCookbook.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SmartCookbookControllerBase : ControllerBase
+{
+}
