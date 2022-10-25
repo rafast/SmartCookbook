c87713bd8c99287fac44de99e16ba04645f33b10
Rafael Telles <rafast.telles@gmail.com>
2022-10-25 15:12:26 -0300

    Add testes de unidade regra de negocio alterar senha

---
+++ tests/TestsUtility/CurrentUser/CurrentUserBuilder.cs
@@ -0,0 +1,35 @@
+﻿using Moq;
+using SmartCookbook.Application.Services.CurrentUser;
+
+namespace TestsUtility.CurrentUser;
+public class CurrentUserBuilder
+{
+    private static CurrentUserBuilder _instance;
+    private readonly Mock<ICurrentUser> _repository;
+
+    private CurrentUserBuilder()
+    {
+        if (_repository == null)
+        {
+            _repository = new Mock<ICurrentUser>();
+        }
+    }
+
+    public static CurrentUserBuilder Instance()
+    {
+        _instance = new CurrentUserBuilder();
+        return _instance;
+    }
+
+    public CurrentUserBuilder GetCurrentUser(SmartCookbook.Domain.Entities.User user)
+    {
+        _repository.Setup(r => r.GetCurrentUser()).ReturnsAsync(user);
+
+        return this;
+    }
+
+    public ICurrentUser Build()
+    {
+        return _repository.Object;
+    }
+}
---
+++ tests/TestsUtility/Repositories/UserUpdateOnlyRepositoryBuilder.cs
@@ -0,0 +1,35 @@
+﻿using Moq;
+using SmartCookbook.Domain.Repositories.User;
+
+namespace TestsUtility.Repositories;
+public class UserUpdateOnlyRepositoryBuilder
+{
+    private static UserUpdateOnlyRepositoryBuilder _instance;
+    private readonly Mock<IUserUpdateOnlyRepository> _repository;
+
+    private UserUpdateOnlyRepositoryBuilder()
+    {
+        if (_repository == null)
+        {
+            _repository = new Mock<IUserUpdateOnlyRepository>();
+        }
+    }
+
+    public static UserUpdateOnlyRepositoryBuilder Instance()
+    {
+        _instance = new UserUpdateOnlyRepositoryBuilder();
+        return _instance;
+    }
+
+    public UserUpdateOnlyRepositoryBuilder GetById(SmartCookbook.Domain.Entities.User user)
+    {
+        _repository.Setup(r => r.GetById(user.Id)).ReturnsAsync(user);
+
+        return this;
+    }
+
+    public IUserUpdateOnlyRepository Build()
+    {
+        return _repository.Object;
+    }
+}
