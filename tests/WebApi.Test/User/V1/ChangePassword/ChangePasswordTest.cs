c2435531bcd55b18f5511ee9d1c62857d80b8ba0
Rafael Telles <rafast.telles@gmail.com>
2022-10-28 17:01:47 -0300

    Add teste de unidade endpoint alterar senha

---
+++ tests/WebApi.Test/User/V1/ChangePassword/ChangePasswordTest.cs
@@ -0,0 +1,59 @@
+﻿using FluentAssertions;
+using SmartCookbook.Comunicacao.Request;
+using SmartCookbook.Exceptions;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using TestsUtility.Requests;
+using Xunit;
+
+namespace WebApi.Test.User.V1.ChangePassword;
+public class ChangePasswordTest : ControllerBase
+{
+    private const string METHOD = "user/change-password";
+
+    private SmartCookbook.Domain.Entities.User _user;
+    private string _password;
+
+    public ChangePasswordTest(SmartCookbookWebApplicationFactory<Program> factory) : base(factory)
+    {
+        _user = factory.GetUser();
+        _password = factory.GetPassword();
+    }
+
+    [Fact]
+    public async Task Validate_Success()
+    {
+        var request = ChangePasswordRequestBuilder.Build();
+        request.OldPassword = _password;
+
+        var token = await Login(_user.Email, _password);
+
+        var response = await PutRequest(METHOD, request, token);
+
+        response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+    }
+
+    [Fact]
+    public async Task Validate_Error_EmptyNewPassword()
+    {
+        var request = new ChangePasswordRequestJson
+        {
+            OldPassword = _password,
+            NewPassword = string.Empty
+        };
+
+        var token = await Login(_user.Email, _password);
+
+        var response = await PutRequest(METHOD, request, token);
+
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+        await using var respondeBody = await response.Content.ReadAsStreamAsync();
+
+        var respondeData = await JsonDocument.ParseAsync(respondeBody);
+
+        var errors = respondeData.RootElement.GetProperty("messages").EnumerateArray();
+        errors.Should().ContainSingle().And.Contain(x => x.GetString().Equals(ResourceErrorMessages.EMPTY_USER_PASSWORD));
+    }
+}
--- tests/WebApi.Test/User/V1/ControllerBase.cs
+++ tests/WebApi.Test/User/V1/ControllerBase.cs
@@ -1,8 +1,10 @@
 ﻿using Newtonsoft.Json;
+using SmartCookbook.Comunicacao.Request;
 using SmartCookbook.Exceptions;
 using System.Globalization;
 using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -22,5 +24,37 @@ public class ControllerBase : IClassFixture<SmartCookbookWebApplicationFactory<P
     {
         var jsonString = JsonConvert.SerializeObject(body);
         return await _client.PostAsync(method, new StringContent(jsonString, Encoding.UTF8, "application/json"));
+    }
+
+    protected async Task<HttpResponseMessage> PutRequest(string method, object body, string token = "")
+    {
+        RequestAuthorize(token);
+        var jsonString = JsonConvert.SerializeObject(body);
+        return await _client.PutAsync(method, new StringContent(jsonString, Encoding.UTF8, "application/json"));
+    }
+
+    protected async Task<string> Login(string email, string password)
+    {
+        var request = new LoginRequestJson
+        {
+            Email = email,
+            Password = password
+        };
+
+        var response = await PostRequest("login", request);
+
+        await using var responseBody = await response.Content.ReadAsStreamAsync();
+
+        var responseData = await JsonDocument.ParseAsync(responseBody);
+
+        return responseData.RootElement.GetProperty("token").ToString();
+    }
+
+    private void RequestAuthorize(string token)
+    {
+        if (!string.IsNullOrWhiteSpace(token))
+        {
+            _client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+        }
     }
 }
